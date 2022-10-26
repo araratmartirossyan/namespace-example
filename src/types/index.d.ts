b345bb7344eddce3fb1834088e51f52750dbd24e
Ararat <myfailemtions@gmail.com>
2022-10-26 13:44:28 +0200

    init

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+declare namespace ComponentsProps {
+  interface HelloWorldProps {
+    name: string;
+    title: string;
+    price: number;
+  }
+}
+
+declare namespace Api {
+  interface LoginResult {
+    fullName: string;
+  }
+}
